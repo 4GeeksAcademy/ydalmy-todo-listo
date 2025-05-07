30cf920c3fd2a960dcfc11bb677533b40a8920d9
ydalmy <ydalmi.arvelaez@gmail.com>
2025-05-07 18:52:03 +0000

    terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,58 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const handleOnChange = (e) => {
+		setInputValue(e.target.value);
+	}
+	const handleOnClick = () => {
+		setTodos([...todos, inputValue]);
+		setInputValue("");
+	}
+	const handleDelete = (index) => {
+		const updateTodos = todos.filter((todos, i)=> i !== index)
+setTodos(updateTodos)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	}
+return (
+	<div className="container">
+		<h2>TODOS </h2>
+		<div clasName="container _input">
+			<input type="text" value={inputValue} onChange={handleOnChange} />
+			<button type="button" className="btn btn-primary" onClick={handleOnClick}> Add</button>
+			</div>
+        
+			<ul>
+				{
+					todos.length > 0 ? todos.map((todo, index) => {
+						return (
+							<li key={index}>{todo}<button type="button" className="btn btn-dark" onClick={() => { handleDelete(index) }}>Delete</button></li>
+						)
+
+
+					}):
+				
+
+
+				<p> No hay tareas aun </p>
+}
+
+			   </ul>
+
+		   </div>
+	
+		
+	
+
+			)
+		}
+	
 
 export default Home;
