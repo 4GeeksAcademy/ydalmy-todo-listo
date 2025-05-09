6a9b51fe9d4850d4069793353a342dfb367bc46f
ydalmy <ydalmi.arvelaez@gmail.com>
2025-05-09 19:32:42 +0000

    corregido

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
@@ -12,23 +11,29 @@ const Home = () => {
 	const handleOnChange = (e) => {
 		setInputValue(e.target.value);
 	}
-	const handleOnClick = () => {
+	const handleOnClick = (evt) => { 
+		evt.preventDefault();
+		if(inputValue.trim()=== ''){
+			alert('Debe contener un valor')
+			return;
+		}
 		setTodos([...todos, inputValue]);
 		setInputValue("");
-	}
-	const handleDelete = (index) => {
-		const updateTodos = todos.filter((todos, i)=> i !== index)
-setTodos(updateTodos)
 
 	}
-return (
-	<div className="container">
-		<h2>TODOS </h2>
-		<div clasName="container _input">
-			<input type="text" value={inputValue} onChange={handleOnChange} />
-			<button type="button" className="btn btn-primary" onClick={handleOnClick}> Add</button>
+	const handleDelete = (index) => {
+		const updateTodos = todos.filter((todos, i) => i !== index)
+		setTodos(updateTodos)
+
+	}
+	return (
+		<div className="container">
+			<h2>TODOS </h2>
+			<div clasName="container _input">
+				<input type="text" value={inputValue} onChange={handleOnChange}  />
+				<button type="button" className="btn btn-primary" onClick={handleOnClick}> Add</button>
 			</div>
-        
+
 			<ul>
 				{
 					todos.length > 0 ? todos.map((todo, index) => {
@@ -37,22 +42,22 @@ return (
 						)
 
 
-					}):
-				
+					}) :
 
 
-				<p> No hay tareas aun </p>
+
+						<p> No hay tareas aun </p>
+				}
+
+			</ul>
+
+		</div>
+
+
+
+
+	)
 }
 
-			   </ul>
-
-		   </div>
-	
-		
-	
-
-			)
-		}
-	
 
 export default Home;
